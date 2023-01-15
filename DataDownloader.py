2b45502419c42026fda80107f9114fa5264ea0d1
Samuel <jukee.oc@gmail.com>
2023-01-16 01:19:01 +0800

    Update DataDownloader.py

--- DataDownloader.py
+++ DataDownloader.py
@@ -1,22 +1,21 @@
 # imports
 from pybit.usdt_perpetual import HTTP
 import pandas as pd
-import numpy as np
 import time
-from scipy.stats import linregress
 session = HTTP("https://api.bybit.com")
 
 # Parameters
 target_trading_pair = 'BTCUSDT'
-interval_int = 240
-file_name = "BTC_USDT_4h.csv"
+interval = '240'
+file_name = 'BTC_USDT_4h.csv'
+start = 1546300800
 
-def get_all_usdt_perceptual(contract_symbol, interval_int, file_name):
-    start = 1546300800
+def get_all_usdt_perceptual(contract_symbol, interval, file_name):
+    
     all_df = []
     
     while True:
-        data = session.query_kline(symbol=contract_symbol, interval=interval_int, limit=200, from_time=start)
+        data = session.query_kline(symbol=contract_symbol, interval=interval, limit=200, from_time=start)
         data_df = pd.DataFrame(data['result'])
         data_df['time'] = pd.to_datetime(data_df['open_time'], unit='s')
         prev = start
@@ -37,4 +36,4 @@ def get_all_usdt_perceptual(contract_symbol, interval_int, file_name):
     
     return data
 
-data = get_all_usdt_perceptual(target_trading_pair, interval_int, file_name)
+data = get_all_usdt_perceptual(target_trading_pair, interval, file_name)
